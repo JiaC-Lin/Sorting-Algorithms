42776a0c212735ec7d34f5fffc612473a0fb4383
JiaC-Lin <jiacong.d.lin@gmail.com>
2023-02-20 22:48:23 -0500

    comments

--- mergeSort.py
+++ mergeSort.py
@@ -17,10 +17,12 @@ class Sort:
             self.mergeSort(left)
             self.mergeSort(right)
 
+            # set variables
             i, j, k = 0, 0, 0
             sizeL = len(left)
             sizeR = len(right)
 
+            # Sort by comparing the smallest of two sets
             while i < sizeL and j < sizeR:
                 if left[i] <= right[j]:
                     arr[k] = left[i]
@@ -30,7 +32,7 @@ class Sort:
                     j += 1
                 k += 1
             
-            # deal with the extra values after one side is finished
+            # deal with the extra values after one set is finished
             while i < sizeL:
                 arr[k] = left[i]
                 i += 1
@@ -40,7 +42,7 @@ class Sort:
                 j += 1
                 k += 1
 
-# Recursive sort 2 utilizing merge function
+# Recursive sort 2 utilizing merge function with O(nlogn) time and O(n) space complexity
     def merge(self, left, right):
         # check if there is nothing to merge: length of one half is 0
         if len(left) == 0:
@@ -52,6 +54,7 @@ class Sort:
         totalSize = len(left) + len(right)
         i, j = 0, 0
 
+        # Sort the 2 sides into one array
         while len(arr) < totalSize:
             if left[i] < right[j]:
                 arr.append(left[i])
@@ -65,6 +68,7 @@ class Sort:
         return arr
     
     def mergeSort2(self, arr:List[int]):
+        # Base Case
         if len(arr) <= 1:
             return arr
         
@@ -73,10 +77,20 @@ class Sort:
         left = arr[:mid]
         right = arr[mid:]
 
+        # Recursively sort each half
         rLeft = self.mergeSort2(left)
         rRight = self.mergeSort2(right)
 
+        # Finally, mergesort the left and right half after each half is sorted
         return self.merge(rLeft, rRight)
+
+# Iterative Sort with O(nlogn) time complexity and O(n) space complexity
+    def iterMerge(self, ):
+        pass
+
+    def iterMergeSort(self, arr:List[int]):
+        n = len(arr)
+
 
 arr = [12, 11, 13, 5, 6, 7]
 Sort().mergeSort(arr)
@@ -96,3 +110,11 @@ for i in range(size):
     print(output[i], end=" ")
 print("\n")
 
+arr = [2, 83, 23, 31, 51, 16]
+Sort().iterMergeSort(arr)
+
+print("Sorted Iterative Array is:")
+size = len(arr)
+for i in range(size):
+    print(arr[i], end=" ")
+print("\n")
