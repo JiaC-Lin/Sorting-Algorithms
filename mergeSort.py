2a914e4d3908d72099cdfdb87e5855dbd4eb8cac
JiaC-Lin <jiacong.d.lin@gmail.com>
2023-02-27 17:45:25 -0500

    mergesort iterative comments and optimizations

--- mergeSort.py
+++ mergeSort.py
@@ -44,7 +44,7 @@ class Sort:
                 k += 1
 
 # Recursive sort 2 utilizing merge function with O(nlogn) time and O(n) space complexity
-    def merge(self, left, right):
+    def recurMerge(self, left:List[int], right:List[int]):
         # check if there is nothing to merge: length of one half is 0
         if len(left) == 0:
             return right
@@ -83,59 +83,65 @@ class Sort:
         rRight = self.mergeSort2(right)
 
         # Finally, mergesort the left and right half after each half is sorted
-        return self.merge(rLeft, rRight)
+        return self.recurMerge(rLeft, rRight)
 
 # Iterative Sort with O(nlogn) time complexity and O(n) space complexity
-# [ l | m ]     [---| r ]   [ l | m ]   [ r ]
-# [ l |---|---| m ]     [---|---| r ]
-# [---|---|---|---|---|---|---]
-    def iterMerge(self, a, l, m, r):
-        # determine the size of left array
+# Start sorting all subarrays of length 1, by definition is already sorted. Then sort all subarrays of length 2 by merging length-1 subarrays. Then sort all subarrays of length 4 by merging length-2 subarrays. Repeat till whole array is sorted.
+# [ l | m ]     [---| r ]       [ l | m ]       [ r ]           //sub_size of 2
+# [ l |---|---| m ]     [---|---| r ]                           //sub_size of 4
+# [---|---|---|---|---|---|---]                                 //sub_size of 8
+    def iterMerge(self, arr:List[int], l:int, m:int, r:int):
+        # determine the size of left and right arrays
         n1 = m - l + 1 
         n2 = r - m 
-        # make an array with space of n1
+        # allocate an array with space of n1 and n2
         Left = [0] * n1 
         Right = [0] * n2 
-        # fill in the 2 mergers
+        # fill in the left and right array with the 2 arrays we want to mergesort
         for i in range(0, n1): 
-            Left[i] = a[l + i] 
+            Left[i] = arr[l + i] 
         for i in range(0, n2): 
-            Right[i] = a[m + i + 1] 
+            Right[i] = arr[m + i + 1] 
     
-        i, j, k = 0, 0, l 
+        # mergesorting 2 arrays back into the original starting at position 'l', similar to recursive merge while in place
+        # Sort by comparing the smallest of two sets
+        i, j = 0, 0 
         while i < n1 and j < n2: 
             if Left[i] <= Right[j]: 
-                a[k] = Left[i] 
+                arr[l] = Left[i] 
                 i += 1
             else: 
-                a[k] = Right[j] 
+                arr[l] = Right[j] 
                 j += 1
-            k += 1
+            l += 1
     
+        # deal with the extra values after one set is finished
         while i < n1: 
-            a[k] = Left[i] 
+            arr[l] = Left[i] 
             i += 1
-            k += 1
-    
+            l += 1
         while j < n2: 
-            a[k] = Right[j] 
+            arr[l] = Right[j] 
             j += 1
-            k += 1
-
+            l += 1
+ 
     def iterMergeSort(self, arr:List[int]):
         n = len(arr)
-        for p in range(2, n + 1):
-            for i in range(0, n + 1 - p):
+        # sub_size start with least partition size of 2^0 = 1 then multiply sub_size by 2 (until sub_size reaches n or higher) since subarray size grows by powers of 2. Start at 2 because with partition size 1 means its already merged with itself and sorted to itself
+        for sub_size in range(2, n):
+            # always start from leftmost and no need to sort the last element as it won't be merging with anything
+            for i in range(0, n - 1):
                 left = i
-                right = left + p - 1
-                mid = (left + right) // 2
-
+                mid = min(left + sub_size - 1, n - 1)
+                right = min(left + 2 * sub_size - 1, n - 1)
+                # final merge should consider unmerged sublist if input arr size is not power of 2
                 self.iterMerge(arr, left, mid, right)
 
-                i += p
-            p *= 2 # for the next 2 mergers
+                i += (sub_size * 2)
+            # increase sub array size by multiplication power of 2 after each merge
+            sub_size *= 2 
 
-arr = [12, 11, 13, 5, 6, 7]
+arr = [12, 11, 13, 5, 6, 7, 1]
 Sort().mergeSort(arr)
 
 print("Sorted Recursive Array is:")
@@ -144,7 +150,7 @@ for i in range(size):
     print(arr[i], end=" ")
 print("\n")
 
-arr = [11, 31, 7, 41, 101, 56, 76, 2]
+arr = [11, 31, 7, 41, 101, 56, 76, 2, 99]
 output = Sort().mergeSort2(arr)
 
 print("Sorted Recursive Array using merge function is:")
