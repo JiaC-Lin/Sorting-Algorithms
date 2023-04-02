8bfb033343cf0c4e78e166b68458d49219534b57
JiaC-Lin <jiacong.d.lin@gmail.com>
2023-04-01 21:40:28 -0400

    comments on algo

--- quickSort.py
+++ quickSort.py
@@ -5,6 +5,8 @@ class Sort:
     def swap(self, arr: List[int], i: int, j: int):
         arr[i], arr[j] = arr[j], arr[i]
     
+    # Partitions by chooosing a pivot to seperate elements less and greater than the pivot. Lastly, place the pivot in their correct index
+    # Returns index of the pivot in it's sorted position
     def partition(self, arr: List[int], low: int, high: int):
         # uses last element as a PIVOT which can be optimized to be random
         pivot = arr[high]
@@ -22,8 +24,9 @@ class Sort:
         return i
 
 # Recursion Quick Sort
-    # worst case: O(n^2)
-    # best and average time complexity: O(nlogn) and worst case space O(n) and best case O(logn)
+    # Best and average time complexity: O(nlogn), worst case: O(n^2)
+    # Best case space complexity: O(logn), worst case: O(n)
+    # Recursively call quicksort where in each instance we partition the pivot using low and high arguments then doing the same quicksorting to both sides
     def recurQuickSort(self, arr: List[int], low: int, high: int):
         # Need base case to exit recursion
         if low < high: 
@@ -34,38 +37,31 @@ class Sort:
             self.recurQuickSort(arr, part + 1, high)
 
 # Iterative Quick Sort
-    # Time complexity of O(nlogn) with space complexity of O(n)
+    # Best and average time complexity: O(nlogn), worst case: O(n^2)
+    # Space complexity: O(n)
+    # Make a stack and push the low and high into stack to partition then we continue a while loop to pop out top two elements from stack into h and l. Using h and l, partition array for to sort the one element. Each time, we push 2 variables into stack if ((partition - 1) > l) or (partition + 1) < h) meaning we the array is not fully sorted. We pop out the next two elements in the stack to repeat the while loop again.
     def iterQuickSort(self, arr:List[int], low: int, high: int):
         # create auxilary stack
-        stack = [0] * (high - low + 1)
-        top = 0
+        stack = []
         # push/fill in stack
-        stack[top] = low
-        top += 1
-        stack[top] = high
+        stack.append(low)
+        stack.append(high)
 
-        while top >= 0:
+        while len(stack) >= 1:
+            print(f"{stack}")
             # pop out high and low then into h and l
-            h = stack[top]
-            top -= 1
-            l = stack[top]
-            top -= 1
+            h = stack.pop()
+            l = stack.pop()
             # set pivot element in their correct position (part index) of sorted array
             part = self.partition(arr, l, h)
             # check if there are elements on left side or right side of part then push right side to stack
             if (part - 1) > l:
-                top += 1
-                stack[top] = l
-
-                top += 1
-                stack[top] = (part - 1)
+                stack.append(l)
+                stack.append(part - 1)
 
             if (part + 1) < h:
-                top += 1
-                stack[top] = part + 1
-
-                top += 1
-                stack[top] = h
+                stack.append(part + 1)
+                stack.append(h)
 
 arr = [2, 3, 14, 31, 61, 9, 18, 19]
 print(f"Unsorted Array is : {arr}")
@@ -73,7 +69,7 @@ N = len(arr)
 Sort().recurQuickSort(arr, 0, N - 1)
 print(f"Sorted Array is: {arr}", end="\n\n")
 
-arr = random.randint(50, size=(7))
+arr = random.randint(50, size=(10))
 print(f"Unsorted Array: {arr}")
 N = len(arr)
 Sort().iterQuickSort(arr, 0 , N - 1)
--- selectionSort.py
+++ selectionSort.py
@@ -1,3 +1,11 @@
 # O(n^2) time with O(1) space complexity
 
 # similar to insertion sort
+from numpy import random
+
+class Sort:
+    def recurSelectionSort(self):
+        pass
+
+A = random.randint(50, size=(7))
+print(f"Unsorted Array is: {A}")
